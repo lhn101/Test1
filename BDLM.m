6cdc759a6748f25bd6be224b128ac1438e9f4c35
Luong Ha Nguyen <luongha.nguyen@gmail.com>
2017-05-29 11:45:02 -0400

    New feature

--- BDLM.m
+++ BDLM.m
@@ -15,7 +15,7 @@ clc
 set(0,'DefaultAxesFontname','Helvetica')                                %Set default font type
 set(0,'DefaultAxesFontSize',20)                                         %Set default font size
 format short g                                                          %Set display format
-% feature 1
+% feature 11
 disp(' ')
 disp('----------------------------------------------------------------------------------------------')
 disp('Bayesian Dynamic Linear Model')
